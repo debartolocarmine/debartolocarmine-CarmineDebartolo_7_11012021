9d05f9775391f364059ca3c6bcfda30fbdc402d8
debartolocarmine <debartolocarmine@gmail.com>
2021-11-18 01:09:00 +0100

    cd-006-refactoring navbar element

--- front/src/components/Navbar/Navbar.js
+++ front/src/components/Navbar/Navbar.js
@@ -3,8 +3,9 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { getBaseApi, getUserSession, getUserInformation } from '../Utils/Tools'
-import logo from '../../components/logo_p-7/icon.png';
-
+// import logo from '../../components/logo_p-7/icon.png';
+import logo from '../../components/logo/icon-left-font-monochrome-white.png';
+import navBg from '../../components/logo/banner.png';
 // Import style
 import './Navbar.scss';
  
@@ -42,30 +43,32 @@ class Navbar extends Component {
 
     let { logged_user } = this.state
     return (
-      <div className="app-navbar">
-        <div className="container">
-          <ul className="app-navbar-wrapper">
-            <img src={logo} className="header-logo" alt="logo" />
-            { logged_user ? (
-              <ul className="app-navbar-wrapper">
-                <li className="app-navbar-user">
-                  {logged_user.portrait &&
-                    <div styles={`background-image: url('${getBaseApi(false)}/images/portraits/${logged_user.portrait}')`} className="user-portrait"></div>
-                  }
-                  <div className="user-name">
-                    <Link to={`/user/${logged_user.username}`} className="nav-link" alt="Profile" title="Profile"><span>{logged_user.prenom}</span><span>{logged_user.nom}</span></Link>
-                  </div>
-                </li>
-                <li><Link to={`/user/${logged_user.username}/edition`} className="nav-link" alt="Profile edition" title="Profile edition"><i className="bi bi-wrench"></i></Link></li>
-                <li><a href="/#" className="nav-link" onClick={this.logout.bind(this)} alt="Déconnexion" title="Déconnexion"><i className="bi bi-door-closed"></i></a></li>
-              </ul>
-            ) : (
-              <ul className="app-navbar-wrapper">
-                <li><Link to={'/connexion'} className="nav-link" alt="Connexion" title="Connexion"><i className="bi bi-person-circle"></i></Link></li>
-                <li><Link to={'/enregistrement'} className="nav-link" alt="Enregistrement" title="Enregistrement"><i className="bi bi-building"></i></Link></li>
-              </ul>
-            )}
-          </ul>
+      <div className="app-navbar" style={{ backgroundImage: `url(${navBg})` }}>
+        <div className="app-navbar-bg">
+          <div className="container">
+            <ul className="app-navbar-wrapper">
+              <img src={logo} className="header-logo" alt="logo" />
+              { logged_user ? (
+                <ul className="app-navbar-wrapper">
+                  <li className="app-navbar-user">
+                    {logged_user.portrait &&
+                      <div styles={`background-image: url('${getBaseApi(false)}/images/portraits/${logged_user.portrait}')`} className="user-portrait"></div>
+                    }
+                    <div className="user-name">
+                      <Link to={`/user/${logged_user.username}`} className="nav-link" alt="Profile" title="Profile"><span>{logged_user.prenom}</span><span>{logged_user.nom}</span></Link>
+                    </div>
+                  </li>
+                  <li><Link to={`/user/${logged_user.username}/edition`} className="nav-link" alt="Profile edition" title="Profile edition"><i className="bi bi-wrench"></i></Link></li>
+                  <li><a href="/#" className="nav-link" onClick={this.logout.bind(this)} alt="Déconnexion" title="Déconnexion"><i className="bi bi-door-closed"></i></a></li>
+                </ul>
+              ) : (
+                <ul className="app-navbar-wrapper">
+                  <li><Link to={'/connexion'} className="nav-link" alt="Connexion" title="Connexion"><i className="bi bi-person-circle"></i></Link></li>
+                  <li><Link to={'/enregistrement'} className="nav-link" alt="Enregistrement" title="Enregistrement"><i className="bi bi-building"></i></Link></li>
+                </ul>
+              )}
+            </ul>
+          </div>
         </div>
       </div>
     )
