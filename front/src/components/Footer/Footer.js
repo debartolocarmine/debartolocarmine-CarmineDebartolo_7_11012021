94da39d6e6719445696dfc9301cab1c8e657fe6c
debartolocarmine <debartolocarmine@gmail.com>
2021-11-18 11:35:25 +0100

    cd-014-Refactor wacg footer

--- front/src/components/Footer/Footer.js
+++ front/src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ class Footer extends Component {
         <div className="app-footer-overlay">
           <div className="container">
             <div className="app-footer-logo">
-              <img src={logo} className="header-logo" alt="logo" />
+              <img src={logo} className="header-logo" alt="Groupomania logo" />
             </div>
           </div>
           <div className="container">
@@ -25,31 +25,31 @@ class Footer extends Component {
                 <h2>À PROPOS</h2>
                 <ul className="cd-ftr-list">
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://openclassrooms.com/fr/terms-conditions" title="Conditions Générales de vente" target="blank">
                       <span className="cd-i"><i className="bi bi-cart2"></i></span>
                       <span className="cd-l">Conditions générales de ventes</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://openclassrooms.com/fr/privacy-policy" title="Politique de Protection des Données Personnelles" target="blank">
                       <span className="cd-i"><i className="bi bi-brush"></i></span>
                       <span className="cd-l">Données personnelles</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://openclassrooms1.myspreadshop.fr/imprint" title=" En toute sérenité avec nos mentions légales" target="blank">
                       <span className="cd-i"><i className="bi bi-building"></i></span>
                       <span className="cd-l">Mentions légales</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link"  href="https://openclassrooms.com/fr/#" title="Cookies" target="blank">
                       <span className="cd-i"><i className="bi bi-emoji-sunglasses"></i></span>
                       <span className="cd-l">Gérer mes cookies</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://blog.openclassrooms.com/" title="Donnez votre avis" target="blank">
                       <span className="cd-i"><i className="bi bi-award"></i></span>
                       <span className="cd-l">Avis</span>
                     </a>
@@ -60,33 +60,28 @@ class Footer extends Component {
                 <h2>SAV & SERVICES</h2>
                 <ul className="cd-ftr-list">
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://openclassrooms.zendesk.com/hc/fr" title="service après-vente" target="blank">
                       <span className="cd-i"><i className="bi bi-brightness-high"></i></span>
                       <span className="cd-l">SAV</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
-                      <span className="cd-i"><i className="bi bi-chat-quote"></i></span>
-                      <span className="cd-l">Besoin d'aide ?</span>
-                    </a>
-                  </li>
-                  <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://openclassrooms.com/fr/about-us" title="Nous somme là pour vous.Contactez-nous!" target="blank">
                       <span className="cd-i"><i className="bi bi-envelope"></i></span>
                       <span className="cd-l">Contactez-nous !</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://openclassrooms.com/forum/" title="Faites vous plaisir avec les Cartes cadeaux" target="blank">
                       <span className="cd-i"><i className="bi bi-gift"></i></span>
                       <span className="cd-l">Cartes cadeaux</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://openclassrooms.com/fr/business" title="Formez et recrutez les talents avec nos formations en ligne.
+" target="blank">
                       <span className="cd-i"><i className="bi bi-bricks"></i></span>
-                      <span className="cd-l">Sécurité</span>
+                      <span className="cd-l">Former et recruter</span>
                     </a>
                   </li>
                 </ul>
@@ -95,33 +90,33 @@ class Footer extends Component {
                 <h2>GROUPOMANIA</h2>
                 <ul className="cd-ftr-list">
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://openclassrooms.com/fr/about-us" title="Découvrez l'équipe d'openclassroom" target="blank">
                       <span className="cd-i"><i className="bi bi-eyeglasses"></i></span>
                       <span className="cd-l">Qui sommes nous ?</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://jobs.openclassrooms.com/fr/" title="Découvrez notre stack technique et rencontrez l'équipe" target="blank">
                       <span className="cd-i"><i className="bi bi-gem"></i></span>
                       <span className="cd-l">Recrutement</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://openclassrooms.com/fr/#" title=" un développement qui répond aux besoins du présent" target="blank">
                       <span className="cd-i"><i className="bi bi-recycle"></i></span>
                       <span className="cd-l">Développement durable</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link active" href="/" title="Paramètres d'accessibilités">
+                    <a className="nav-link active" href="https://openclassrooms.com/fr/p/laccessibilite-sur-la-plateforme-openclassrooms" title="L'accessibilité sur la plateforme OpenClassrooms" target="blank">
                       <span className="cd-i"><i className="bi bi-eye-slash"></i></span>
                       <span className="cd-l">Paramètres d'accessibilités</span>
                     </a>
                   </li>
                   <li className="cd-ftr-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="https://openclassrooms1.myspreadshop.fr/" title="Tous les produits en boutique" target="blank">
                       <span className="cd-i"><i className="bi bi-recycle"></i></span>
-                      <span className="cd-l">Plan du site</span>
+                      <span className="cd-l">Shop</span>
                     </a>
                   </li>
                 </ul>
